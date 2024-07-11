80ca62039411d6c54f1b63510ddbbedeeb485c3b
Kartikay <kartikayjoshiprivate@gmail.com>
2024-07-11 14:36:37 +0530

    add more colors

--- 02-print.sh
+++ 02-print.sh
@@ -40,52 +40,52 @@ echo -e "\e[36mHello In CYAN Color\e[0m"
 
 
 
-echo -e "Default \e[39mDefault"
+echo -e "Default \e[39mDefault\e[0m"
 
 
-echo -e "Default \e[30mBlack"
+echo -e "Default \e[30mBlack\e[0m"
 
 
-echo -e "Default \e[31mRed"
+echo -e "Default \e[31mRed\e[0m"
 
 
-echo -e "Default \e[32mGreen"
+echo -e "Default \e[32mGreen\e[0m"
 
 
-echo -e "Default \e[33mYellow"
+echo -e "Default \e[33mYellow\e[0m"
 
 
-echo -e "Default \e[34mBlue"
+echo -e "Default \e[34mBlue\e[0m"
 
 
-echo -e "Default \e[35mMagenta"
+echo -e "Default \e[35mMagenta\e[0m"
 
 
-echo -e "Default \e[36mCyan"
+echo -e "Default \e[36mCyan\e[0m"
 
 
-echo -e "Default \e[37mLight gray"
+echo -e "Default \e[37mLight gray\e[0m"
 
 
-echo -e "Default \e[90mDark gray"
+echo -e "Default \e[90mDark gray\e[0m"
 
 
-echo -e "Default \e[91mLight red"
+echo -e "Default \e[91mLight red\e[0m"
 
 
-echo -e "Default \e[92mLight green"
+echo -e "Default \e[92mLight green\e[0m"
 
 
-echo -e "Default \e[93mLight yellow"
+echo -e "Default \e[93mLight yellow\e[0m"
 
 
-echo -e "Default \e[94mLight blue"
+echo -e "Default \e[94mLight blue\e[0m"
 
 
-echo -e "Default \e[95mLight magenta"
+echo -e "Default \e[95mLight magenta\e[0m"
 
 
-echo -e "Default \e[96mLight cyan"
+echo -e "Default \e[96mLight cyan\e[0m"
 
 
 
