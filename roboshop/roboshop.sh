a0f3b54ed62676709c13031b62d35fd888a1c9ec
Kartikay <kartikayjoshiprivate@gmail.com>
2024-07-24 12:40:43 +0530

    Adding components

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 USER_UID=$(id -u)
-if [ $(USER_UID) -ne 0 ]; then
+
+if [ ${USER_UID} -ne 0 ]; then
   echo -e "-e[1;31mYou should be a root user to perform this script\e[0m"
   exit
 fi
