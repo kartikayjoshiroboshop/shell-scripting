c7f22573d736b8a6245ac771692d8dfe3c400d89
Kartikay <kartikayjoshiprivate@gmail.com>
2024-07-24 12:45:52 +0530

    Adding components

--- roboshop/roboshop.sh
+++ roboshop/roboshop.sh
@@ -3,7 +3,7 @@
 USER_UID=$(id -u)
 
 if [ ${USER_UID} -ne 0 ]; then
-  echo -e "-e[1;31mYou should be a root user to perform this script\e[0m"
+  echo -e "\e[1;31mYou should be a root user to perform this script\e[0m"
   exit
 fi
 
@@ -14,8 +14,8 @@ if  [ -z "$COMPONENT" ]; then
   exit
 fi
 
-if [ -e components/${COMPONENT}.sh ]; then
-  echo -e "\e[1;31mGiven component does not exits\e[0m"
+if [ ! -e components/${COMPONENT}.sh ]; then
+  echo -e "\e[1;31mGiven component script does not exits\e[0m"
   exit
 fi
 
