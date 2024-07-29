ca6cdfdd699b25e4df5b8cf842a24fa07831e720
Kartikay <kartikayjoshiprivate@gmail.com>
2024-07-29 12:19:05 +0530

    Simple ansible

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -36,9 +36,34 @@
 
 ##Check whether the script is running as root user or not
 
-echo frontend setup
+echo Frontend setup
 
-#yum install nginx -y
+yum install nginx -y
+if [ $? -ne 0 ]; then
+  echo "Nginx install failed"
+  exit 1
+fi
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
+cd /usr/share/nginx/html
+
+if [ $? -ne 0 ]; then
+  echo "Download component failed"
+  exit 1
+fi
+
+rm -rf *
+unzip /tmp/frontend.zip
+
+
+ mv frontend-main/* .
+ mv static/* .
+rm -rf frontend-master static README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+systemctl enable nginx
+
+systemctl start nginx
 
 
 
